a0ed227c284e0864aaa415bd743e177e926c1b14
Andrey Minin <ewkjf@mail.ru>
2019-05-29 16:34:55 +0300

    Initial Commit

---
+++ Task tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task tracker
+//
+//  Created by Andrey Minin on 29/05/2019.
+//  Copyright © 2019 Andrey Minin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
